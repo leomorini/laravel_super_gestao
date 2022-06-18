3edd41331987ac32d4e8262d4f8d9889f2d38773
Leonardo Morini <leo.morini.nn@gmail.com>
2022-06-18 14:20:41 -0300

    adicionando factory

--- database/seeds/SiteContatoSeeder.php
+++ database/seeds/SiteContatoSeeder.php
@@ -12,12 +12,14 @@ class SiteContatoSeeder extends Seeder
      */
     public function run()
     {
-        $contato = new SiteContato();
-        $contato->nome = 'Sistema SG';
-        $contato->telefone = '(11) 99999-8888';
-        $contato->email = 'contato@sg.com.br';
-        $contato->motivo_contato = 1;
-        $contato->mensagem = 'Seja bem-dinvo ao sistema Super Gestão';
-        $contato->save();
+        // $contato = new SiteContato();
+        // $contato->nome = 'Sistema SG';
+        // $contato->telefone = '(11) 99999-8888';
+        // $contato->email = 'contato@sg.com.br';
+        // $contato->motivo_contato = 1;
+        // $contato->mensagem = 'Seja bem-dinvo ao sistema Super Gestão';
+        // $contato->save();
+
+        factory(SiteContato::class, 100)->create();
     }
 }
