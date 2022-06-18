09feb26eec641b6cb9340fba9ec53da97feb5111
Leonardo Morini <leo.morini.nn@gmail.com>
2022-06-17 21:00:23 -0300

    Adicionando novos seeders

--- app/SiteContato.php
+++ app/SiteContato.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class SiteContato extends Model
 {
-    //
+    //Habilita que possamos usar as essas colunas nas funções/métodos estáticos do Model
+    protected $fillable = [
+        'nome',
+        'telefone',
+        'email',
+        'motivo_contato',
+        'mensagem'
+    ];
 }
